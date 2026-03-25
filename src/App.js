1ecb221d4419762ac7584b3e83243ded5b98f18f
jwaher-x <jawaher.5alidfnn@gmail.com>
2026-03-25 19:35:36 +0300

    fix layout for smaller screens

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar'; 
 import MainWelcome from './pages/MainWelcome';   
-
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar /> 
         <Routes>
-          <Route path="/" element={<MainWelcome />} />  
+          <Route path="/" element={<MainWelcome />} />
         </Routes>
       </div>
     </Router>
